11c7c8370e8ecbae8d9b8588687dc9241fd4fa4e
st-effen <st.effen@gmx.de>
2022-05-04 17:28:41 +0200

    Dome fixes in deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -58,25 +58,25 @@ if ($isWindows) {
   # Linux
   # currently no multiarch build
   #if ($env:ARCH -eq "arm64v8") {
-	  if ($nev:ARCH -eq "amd64") {
+  if ($env:ARCH -eq "amd64") {
     # The last in the build matrix
     docker -D manifest create "$($image):$env:APPVEYOR_REPO_TAG_NAME" `
-      "$($image):linux-amd64-$env:APPVEYOR_REPO_TAG_NAME" `
+      "$($image):linux-amd64-$env:APPVEYOR_REPO_TAG_NAME" 
       #"$($image):linux-arm64v8-$env:APPVEYOR_REPO_TAG_NAME" `
-      "$($image):windows-amd64-$env:APPVEYOR_REPO_TAG_NAME"
+      #"$($image):windows-amd64-$env:APPVEYOR_REPO_TAG_NAME"
 
     #docker manifest annotate "$($image):$env:APPVEYOR_REPO_TAG_NAME" "$($image):linux-arm64v8-$env:APPVEYOR_REPO_TAG_NAME" --os linux --arch arm64 --variant v8
-	docker manifest annotate "$($image):$env:APPVEYOR_REPO_TAG_NAME" "$($image):linux-amd64-$env:APPVEYOR_REPO_TAG_NAME" --os linux --arch amd64
+    docker manifest annotate "$($image):$env:APPVEYOR_REPO_TAG_NAME" "$($image):linux-amd64-$env:APPVEYOR_REPO_TAG_NAME" --os linux --arch amd64
     docker manifest push "$($image):$env:APPVEYOR_REPO_TAG_NAME"
 
     Write-Host "Pushing manifest $($image):latest"
     docker -D manifest create "$($image):latest" `
-      "$($image):linux-amd64-$env:APPVEYOR_REPO_TAG_NAME" `
+      "$($image):linux-amd64-$env:APPVEYOR_REPO_TAG_NAME" 
       #"$($image):linux-arm64v8-$env:APPVEYOR_REPO_TAG_NAME" `
-      "$($image):windows-amd64-$env:APPVEYOR_REPO_TAG_NAME"
+      #"$($image):windows-amd64-$env:APPVEYOR_REPO_TAG_NAME"
 
     #docker manifest annotate "$($image):latest" "$($image):linux-arm64v8-$env:APPVEYOR_REPO_TAG_NAME" --os linux --arch arm64 --variant v8
-	docker manifest annotate "$($image):latest" "$($image):linux-amd64-$env:APPVEYOR_REPO_TAG_NAME" --os linux --arch amd64
+    docker manifest annotate "$($image):latest" "$($image):linux-amd64-$env:APPVEYOR_REPO_TAG_NAME" --os linux --arch amd64
     docker manifest push "$($image):latest"
 
   }
