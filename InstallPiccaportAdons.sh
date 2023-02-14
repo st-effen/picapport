21424cfbdc3231db1882acaa7ac4d4081ed32361
st-effen <st.effen@gmx.de>
2023-02-14 11:29:56 +0100

    Initial checkin

---
+++ InstallPiccaportAdons.sh
@@ -0,0 +1,7 @@
+
+
+#Download Video Plugin
+https://www.picapport.de/plugins/downloads/PicApportVideoThumbnailPlugin.zip
+
+#Download MetaDataAnalyzer
+https://www.picapport.de/prepare_download-add-ons.php?dlfile=%2fpagpExifToolSimpleInfo-1.0.0.zip
